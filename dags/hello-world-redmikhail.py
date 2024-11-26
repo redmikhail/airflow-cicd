032a66297472c2e11baeeae0cea46f5a09a03240
redmikhail <mmikhail@redhat.com>
2024-11-25 23:05:09 -0500

    Dag fix

--- dags/hello-world-redmikhail.py
+++ dags/hello-world-redmikhail.py
@@ -3,7 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 def helloWorld():
-    print(‘Hello World’)
+    print("Hello World")
           
 with DAG(dag_id="hello_world_dag",
          start_date=datetime(2021,1,1),
